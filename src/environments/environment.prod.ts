7625437f621360b9b1159fa13e5f7b07c3d5d0e0
Trilok <trilok@gaiasys.in>
2021-05-23 20:19:27 +0530

    prod environemnt

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD34TTcPZs8TQT27SUHGxWxAGJK35fJFdc",
+    authDomain: "angular-firebase-todo-62f3e.firebaseapp.com",
+    databaseURL: "https://angular-firebase-todo-62f3e.firebaseio.com",
+    projectId: "angular-firebase-todo-62f3e",
+    storageBucket: "angular-firebase-todo-62f3e.appspot.com",
+    messagingSenderId: "821307157468",
+    appId: "1:821307157468:web:245d81e638f00183c9fd19",
+    measurementId: "G-T3MXD9N09M"
+  }
 };
